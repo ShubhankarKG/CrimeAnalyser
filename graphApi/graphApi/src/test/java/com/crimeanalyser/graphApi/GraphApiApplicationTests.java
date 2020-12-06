1fc59c03fae435f13bf37d916787af3ee290dd1f
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-06 13:39:55 +0530

    Added spring neo4j boilerplate

---
+++ graphApi/graphApi/src/test/java/com/crimeanalyser/graphApi/GraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimeanalyser.graphApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
