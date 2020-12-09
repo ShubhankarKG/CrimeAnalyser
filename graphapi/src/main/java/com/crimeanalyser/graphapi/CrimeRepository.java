646e2022cdc1da2d144dc53067255c08251ad05d
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-09 16:26:02 +0530

    Changed crime schema

--- graphapi/src/main/java/com/crimeanalyser/graphapi/CrimeRepository.java
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/CrimeRepository.java
@@ -1,50 +1,12 @@
 package com.crimeanalyser.graphapi;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.neo4j.annotation.Query;
-import org.springframework.data.neo4j.annotation.QueryResult;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface CrimeRepository extends Neo4jRepository<Crime, Long> {
-  List<Crime> findByType(@Param("type") String type);
-
-  @Query("MATCH (c:Crime) RETURN c.type, COUNT(c) AS total ORDER BY c.type")
-  List<CrimeCount> countCrimesByType();
-
-  @Query("MATCH (c:Crime)-[r:OCCURED_IN]->(l:Location) WHERE l.location=$location RETURN c")
-  List<Crime> getCrimesInLocation(@Param("location") String location);
-
-  @Query("MATCH (c:Crime)-[r:OCCURED_IN]->(l:Location) WHERE r.year=$year RETURN c")
-  List<Crime> getCrimesInYear(@Param("year") Integer year);
-
-  @Query(value = "MATCH (c:Crime)-[r:OCCURED_IN]->(l:Location) WHERE r.year=$year AND l.location=$location RETURN c")
-  List<Crime> getCrimesInLocationAndYear(@Param("year") Integer year, @Param("location") String location);
-}
-
-class CrimeCount {
-  private String type;
-  private Long total;
-
-  public void setType(String crimeType) {
-    this.type = type;
-  }
-
-  public void setTotal(Long total) {
-    this.total = total;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public Long getTotal() {
-    return total;
-  }
+  List<Crime> findByYear(@Param("year") Long year);
+  List<Crime> findByLocation(@Param("location") String location);
+  List<Crime> findByYearAndLocation(@Param("year") Long year, @Param("location") String location);
 }
--- graphapi/src/main/java/com/crimeanalyser/graphapi/Location.java
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/Location.java
@@ -4,26 +4,14 @@ import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @NodeEntity
 public class Location {
   
   @Id @GeneratedValue private Long id;
   private String location;
-
-  private Location(){}
-  public Location(String location) {
-    this.location = location;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public String getLocation() {
-    return location;
-  }
-
-  public void setLocation(String location) {
-    this.location = location;
-  }
 }
