31b437a2762988d41797583d73ae58404aa5018a
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-10 12:03:07 +0530

    Added heatmap api

---
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/LocationController.java
@@ -0,0 +1,55 @@
+package com.crimeanalyser.graphapi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController()
+@RequestMapping("/queries/locations")
+public class LocationController {
+  
+  @Autowired
+  private Driver driver;
+
+  private final String COORD = "coordinates";
+
+  @GetMapping(value = "/getLocationsWithCrimeInYear", produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Map<String, Object>> getLocationsWithCrimesInYear(
+    @RequestParam Integer year,
+    @RequestParam String crime
+  ) {
+    try(Session session = driver.session()) {
+      List<Map<String, Object>> list = new ArrayList<>();
+      session.readTransaction(tx -> {
+        tx.run("MATCH (c:Crime)-[:OCCURED_IN]->(l:Location) WHERE c.year = " + year + " AND l.coordinates IS NOT NULL RETURN l, c;")
+        .stream().forEach(record -> {
+          Map<String, Object> map = new HashMap<>();
+
+          PointObject obj = new PointObject(
+            record.get("l").get(COORD).asPoint().x(),
+            record.get("l").get(COORD).asPoint().y()
+          );
+
+          map.put(COORD, obj);
+          map.put("location", record.get("l").get("location").asObject());
+          map.put(crime, record.get("c").get(crime).asObject());
+          list.add(map);
+        });
+        return list;
+      });
+      return list;
+    }
+  }
+}
--- graphapi/src/main/java/com/crimeanalyser/graphapi/LocationRepository.java
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/LocationRepository.java
@@ -1,7 +1,10 @@
 package com.crimeanalyser.graphapi;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,5 +12,4 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin
 public interface LocationRepository extends Neo4jRepository<Location, Long> {
   List<Location> findByLocation(@Param("location") String location);
-  List<Location> findAll();
 }
