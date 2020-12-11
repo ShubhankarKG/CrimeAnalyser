78852bfcf47841ae77c07b5773d955f16fb43632
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-11 16:37:18 +0530

    added api for getting people who know a criminal

--- graphapi/src/main/java/com/crimeanalyser/graphapi/PersonController.java
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -34,6 +35,7 @@ public class PersonController {
 
           map.put("name", record.get("p").get("name").asObject());
           map.put("gender", record.get("p").get("gender").asObject());
+          map.put("age", record.get("p").get("age").asObject());
           map.put("locaion", record.get("p").get("location").asObject());
           map.put("crime", record.get("crime").asObject());
           map.put("year", record.get("year").asObject());
@@ -44,6 +46,34 @@ public class PersonController {
       });
 
       result.put("criminals", list);
+      return result;
+    }
+  }
+
+  @GetMapping("/getPeopleWhoKnowCriminal")
+  public Map<String, Object> getPeopleWhoKnowCriminal(
+    @RequestParam String name
+  ) {
+    try(Session session = driver.session()) {
+      Map<String, Object> result = new HashMap<>();
+      List<Map<String, Object>> list = new ArrayList<>();
+      
+      session.readTransaction(tx -> {
+        tx.run("MATCH (p:Person)-[:KNOWS]->(c:Person) WHERE c.name = \"" + name + "\" RETURN p;")
+        .stream().forEach(record -> {
+          Map<String, Object> map = new HashMap<>();
+
+          map.put("name", record.get("p").get("name").asObject());
+          map.put("gender", record.get("p").get("gender").asObject());
+          map.put("age", record.get("p").get("age").asObject());
+          map.put("locaion", record.get("p").get("location").asObject());
+
+          list.add(map);
+        });
+        return list;
+      });
+
+      result.put("people", list);
       return result;
     }
   }
