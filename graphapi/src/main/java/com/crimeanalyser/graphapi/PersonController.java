9359cb0e7189ba4ddb7cd4df1ed35e51a62ece30
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-11 16:21:55 +0530

    Added api for getting criminals and fixed access error

---
+++ graphapi/src/main/java/com/crimeanalyser/graphapi/PersonController.java
@@ -0,0 +1,51 @@
+package com.crimeanalyser.graphapi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/queries/people")
+public class PersonController {
+  
+  @Autowired
+  private Driver driver;
+
+  @GetMapping("/getCriminals")
+  public Map<String, Object> getCriminals() {
+    try(Session session = driver.session()) {
+      Map<String, Object> result = new HashMap<>();
+      List<Map<String, Object>> list = new ArrayList<>();
+      
+      session.readTransaction(tx -> {
+        tx.run("MATCH (p:Person)-[l:COMMITED]->(c:Crime) RETURN p, l.type as crime, c.year as year;")
+        .stream().forEach(record -> {
+          Map<String, Object> map = new HashMap<>();
+
+          map.put("name", record.get("p").get("name").asObject());
+          map.put("gender", record.get("p").get("gender").asObject());
+          map.put("locaion", record.get("p").get("location").asObject());
+          map.put("crime", record.get("crime").asObject());
+          map.put("year", record.get("year").asObject());
+
+          list.add(map);
+        });
+        return list;
+      });
+
+      result.put("criminals", list);
+      return result;
+    }
+  }
+
+}
